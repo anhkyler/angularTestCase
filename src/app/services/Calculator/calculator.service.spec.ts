3e2d80fb4ea3c217d188414708b823f3e520d5e8
hieuphso <hieuphso@gmail.com>
2023-07-27 21:04:49 -0400

    using beforeEach to replace duplicated codes

--- src/app/services/Calculator/calculator.service.spec.ts
+++ src/app/services/Calculator/calculator.service.spec.ts
@@ -5,31 +5,42 @@ import { LoggerService } from '../Logger/logger.service';
 
 describe('CalculatorService', () => {
   let service: CalculatorService;
-
+  let mockLoggerSerice: any;
+  let calculator: CalculatorService;
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(CalculatorService);
+    mockLoggerSerice = jasmine.createSpyObj('LoggerService',['log']);
+    calculator = new CalculatorService(mockLoggerSerice);
+    // TestBed.configureTestingModule({});
+    // service = TestBed.inject(CalculatorService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+  // it('should be created', () => {
+  //   expect(service).toBeTruthy();
+  // });
 
   it('should add two number', ()=> {
-    let loggerSerice = new LoggerService();
-    spyOn(loggerSerice,'log');
-    const calculator = new CalculatorService(loggerSerice);
     let result = calculator.add(2,2);
     expect(result).toBe(4);
-    expect(loggerSerice.log).toHaveBeenCalledTimes(1);
+    expect(mockLoggerSerice.log).toHaveBeenCalledTimes(1);
   });
-  it('should - two number',()=> {    
-    let loggerSerice = jasmine.createSpyObj('loggerService',['log']);
-    // spyOn(loggerSerice,'log')
-    //proxy project
-    const calculator = new CalculatorService(loggerSerice);
-    let result = calculator.substract(2,2);
-    expect(result).toBe(0);
-    expect(loggerSerice.log).toHaveBeenCalledTimes(1);
-  });
+  
+
+  // it('should add two number', ()=> {
+  //   let loggerSerice = new LoggerService();
+  //   spyOn(loggerSerice,'log');
+  //   const calculator = new CalculatorService(loggerSerice);
+  //   let result = calculator.add(2,2);
+  //   expect(result).toBe(4);
+  //   expect(loggerSerice.log).toHaveBeenCalledTimes(1);
+  // });
+
+  // it('should - two number',()=> {    
+  //   let loggerSerice = jasmine.createSpyObj('loggerService',['log']);
+  //   // spyOn(loggerSerice,'log')
+  //   //proxy project
+  //   const calculator = new CalculatorService(loggerSerice);
+  //   let result = calculator.substract(2,2);
+  //   expect(result).toBe(0);
+  //   expect(loggerSerice.log).toHaveBeenCalledTimes(1);
+  // });
 });
